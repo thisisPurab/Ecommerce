b42b328ec317c64452fc273178b2643524f4b222
atmossnowy <purabrtamboli@gmail.com>
2024-04-05 15:44:31 +0530

    Frontend Added

--- client/src/firebaseconfig.js
+++ client/src/firebaseconfig.js
@@ -3,17 +3,17 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDy5mNp33WksgZBDEH1_6fW-BlT75rpVW8",
-    // apiKey: process.env.WT_ECOMMERCE_FIREBASE_API_KEY,
-    authDomain: "ecommerce-70a8c.firebaseapp.com",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 
-    projectId: "ecommerce-70a8c",
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 
-    storageBucket: "ecommerce-70a8c.appspot.com",
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 
-    messagingSenderId: "541599574903",
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 
-    appId: "1:541599574903:web:7b5b38cb7a74e42ab70f72",
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
